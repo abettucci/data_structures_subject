a1f1ff51d7aba0dcd2f8637edd75bb3fa3dc24c3
abettucci <89608756+abettucci@users.noreply.github.com>
2022-12-03 00:32:53 -0300

    Add files via upload

---
+++ Clases/TP1 Introduccion/tp1ej1.py
@@ -0,0 +1,51 @@
+# # Mostrar menu
+# # Ver como hacer para volver al menu una vez terminada cada opcion seleccionada
+
+# usuarios = [] #lista de usuarios que va a contener sus datos y luego sus calculos
+# tramiteCorriendo = 1
+# #while tramiteCorriendo == 1:
+# opcion = int(input("Seleccione una opcion:\n1.Registro\n2.Examen\n3.Verificacion\n4.Salir\n"))
+#     #chequear que solo se ingrese un numero entre 1 y 4
+
+# # Panel Registro: 1) pedir DNI 2) codigo empleado 3) tipo licencia 4) generar nro tramite (que no se repita)
+# while opcion>1 and opcion<4:
+#         if opcion == 1:
+#                 #cargarDatos(usuarios)
+#                 #chequeo q no exista ya el DNI
+#            # elif opcion == 2:
+#             resultadoTeorico = int(input("Ingrese nota examen teorico del 10 al 100: "))
+#                                 # chequear que ingrese valor entre 10 y 100
+#             resultadoPractico = int(input("Ingrese nota examen practico del 10 al 100: "))
+#                                 # chequear que ingrese valor entre 10 y 100
+#             if resultadoTeorico<10:
+#                 print("Error! Nota teorica mal ingresada")
+#                 exit()
+#             elif resultadoPractico>100:
+#                 print("Error! Nota practico mal ingresada")
+#                 exit()
+#             else:
+#                 promedioExamenes = (resultadoTeorico + resultadoPractico)/2
+#                 nroTram = str(ingresoDNI)&ingresoCodigoEmp&ingresoLicencia
+#                 registroAbierto = False
+#                                 #que haga el calculo solo cuando estan bien ambos datos
+                
+       
+
+    
+# Funciones
+    #Lista de DNI
+
+"""def cargarDatos(usuarios):
+    usuarios[0][0] = int(input("Ingrese su DNI: "))
+    #chequear que no escriba mal el DNI
+    usuarios[0][1] = int(input("Ingrese su codigo de empleado: ")) 
+    #chequear que no escriba mal el codigo de empleado
+    usuarios[0][2] = input("Ingrese tipo de licencia (PAR = particular, PRO = profesional): ")
+    #chequear que se ponga una o la otra nomas """
+
+
+
+
+
+
+
